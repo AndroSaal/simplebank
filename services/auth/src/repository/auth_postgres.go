7d9568b7a6552109676e08f8fb99e80af0e83fd1
AndroSaal <89251409398@mail.ru>
2024-11-29 21:27:33 +0300

    add get and add user - methods of repository level

---
+++ services/auth/src/repository/auth_postgres.go
@@ -0,0 +1,44 @@
+package repository
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/AndtoSaal/simplebank/services/auth/src/entities/models"
+	"github.com/jmoiron/sqlx"
+)
+
+type AuthPostgresDB struct {
+	db *sqlx.DB
+}
+
+func NewAuthPostgresRepo(db *sqlx.DB) *AuthPostgresDB {
+	return &AuthPostgresDB{db: db}
+}
+
+func (p *AuthPostgresDB) SaveUser(
+	ctx context.Context, email string, passwordHash []byte) (userId int, err error) {
+
+	var id int
+
+	query := fmt.Sprintf("INSERT INTO %s (name, username, password_hash) VALUES ($1, $2, $3) RETURNING id", usersTable)
+	row := p.db.QueryRow(query, email, string(passwordHash))
+
+	if err := row.Scan(&id); err != nil {
+		return 0, err
+	}
+	return id, nil
+}
+
+// TODO : Добавить ошибки
+func (r *AuthPostgresDB) GetUser(username, password string) (models.User, error) {
+
+	var (
+		user models.User
+	)
+
+	query := fmt.Sprintf("SELECT * FROM %s WHERE username=$1 AND password_hash=$2", usersTable)
+	err := r.db.Get(&user, query, username /*$1 в query*/, password /*$2 в query*/)
+
+	return user, err
+}
--- services/auth/src/repository/repository.go
+++ services/auth/src/repository/repository.go
@@ -1,6 +1,20 @@
 package repository
 
-//реализация userRepositoryHandler иинтерфейса из сервисного слоя
-type userRepositoryHandler struct {
-	
+import "context"
+
+// интерфейс, которым определяющий тип, которым должна обладать конкретная бд
+// чтобы его реализовывать
+type Repository interface {
+	SaveUser(ctx context.Context, email string, password_Hash []byte) (userId int, err error)
+	GetUser(ctx context.Context, email string) (userId int, err error)
+}
+
+// тип, релизующий интерфейс сервисвного слоя UserRepository
+type AuthUserRepo struct {
+	repo Repository
+}
+
+// конструктор типа AuthUserRepo
+func NewAuthUserHandler(repo Repository) *AuthUserRepo {
+	return &AuthUserRepo{repo: repo}
 }
