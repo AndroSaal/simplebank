bc81e6ffd121811b4d66ce5450ff1757d3315309
AndroSaal <89251409398@mail.ru>
2024-12-04 00:24:09 +0300

    make something

--- services/auth/src/repository/postgres_repo.go
+++ services/auth/src/repository/postgres_repo.go
@@ -16,11 +16,10 @@ const (
 func NewPostgresDB(cfg config.DatabaseConfig) (*sqlx.DB, error) {
 	//заполняем структурку в конструкторе
 	db, err := sqlx.Open("postgres", fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=%s",
-		cfg.Host, cfg.Port, cfg.UserName, cfg.Password, cfg.Database, cfg.SSLMode))
+		"postgres_auth", cfg.Port, cfg.UserName, cfg.Password, "postgres", "disable"))
 	if err != nil {
 		return nil, err
 	}
-	fmt.Printf("struct %v\n", cfg)
 
 	//методом Ping проверяем, можем ли мы достучаться до нашей БД
 	if err = db.Ping(); err != nil {
