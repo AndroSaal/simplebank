edeb20f7144c60a337399a0d945e7ac9414839e5
AndroSaal <89251409398@mail.ru>
2024-12-05 22:00:41 +0300

    add test migration

---
+++ services/auth/unit_tests/migration_test/000001_init.up.sql
@@ -0,0 +1,9 @@
+CREATE TABLE migration_tests (
+    id SERIAL PRIMARY KEY,
+    email VARCHAR(255) NOT NULL,
+    passsword_hash VARCHAR(255) NOT NULL
+);
+
+INSERT INTO migration_tests (id, email, passsword_hash)  
+VALUES (1, 'test', 'test-secret') 
+ON CONFLICT DO NOTHING;
